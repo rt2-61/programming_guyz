693ea039dc2d7df886b7fc30d285ca3dafb1948d
LoveDoro <bulfrat@gmail.com>
2021-09-29 13:51:20 +0300

    2

--- labnew2/Unit2.cpp
+++ labnew2/Unit2.cpp
@@ -31,6 +31,7 @@ void __fastcall TForm2::Button1Click(TObject *Sender)
 Memo1->Lines->Add("Zhuravlev");
 	Memo1->Lines->Add("Masnevamycomp");
 	Memo1->Lines->Add("Osipovmycomp");
-    Memo1->Lines->Add("Kugashovmycomp");
+        Memo1->Lines->Add("osipov");
+
 }
 //---------------------------------------------------------------------------
